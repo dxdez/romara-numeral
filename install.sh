bc82aaf5bdee67e318b0ebe06baf449736332294
dylanxhernandez <dylan.x.h.developer@gmail.com>
2025-06-07 00:28:07 -0400

    chore: setup folder for install/uninstall script

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-sudo cp ohhai /usr/local/bin/
-sudo chmod +x /usr/local/bin/ohhai
-echo "'ohhai' installed."
+sudo cp romara /usr/local/bin/
+sudo chmod +x /usr/local/bin/romara
+echo "'romara' installed."
